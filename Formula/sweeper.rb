5fa036ca445bb421593bace5950a6554eea0817c
danorul9 <danorul9@gmail.com>
2026-05-10 20:41:09 +1000

    Fix Homebrew formula: hardcode v0.1.0, use universal binary

--- Formula/sweeper.rb
+++ Formula/sweeper.rb
@@ -1,18 +1,12 @@
 class Sweeper < Formula
   desc "macOS app leftover detector & cleaner"
   homepage "https://github.com/danorul9/sweeper"
-  version "{{.Version}}"
+  version "0.1.0"
   license "MIT"
 
   on_macos do
-    on_arm do
-      url "https://github.com/danorul9/sweeper/releases/download/v#{version}/sweeper-v#{version}-darwin-arm64"
-      sha256 "{{.SHA256}}"
-    end
-    on_intel do
-      url "https://github.com/danorul9/sweeper/releases/download/v#{version}/sweeper-v#{version}-darwin-amd64"
-      sha256 "{{.SHA256}}"
-    end
+    url "https://github.com/danorul9/sweeper/releases/download/v#{version}/sweeper-#{version}-darwin-all"
+    sha256 "2d0516abd0ec90222b4b00c350f450cce1a0dc3bf53f940ea72333c713a80833"
   end
 
   def install
