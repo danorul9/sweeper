d62529e1b0026bb0af57134430d7d940651798d5
danorul9 <danorul9@gmail.com>
2026-06-13 21:52:17 +1000

    Update Formula SHA256 for v0.5.0

--- Formula/sweeper.rb
+++ Formula/sweeper.rb
@@ -1,12 +1,12 @@
 class Sweeper < Formula
   desc "macOS app leftover detector & cleaner"
   homepage "https://github.com/danorul9/sweeper"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     url "https://github.com/danorul9/sweeper/releases/download/v#{version}/sweeper-#{version}-darwin-all"
-    sha256 "acfaa914a3bb891ecb106726bc0582a2fd3d593bc0ba012f438d2d69af0f36c6"
+    sha256 "a98bbbb0b91c40e0a3f816d387b19fef061844be7770d60644921d428bc2a3f6"
   end
 
   def install
